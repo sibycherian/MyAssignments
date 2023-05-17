8324916f53d1a3184d1c8ac6ec44c94fbb6a5903
sibycherian <133909656+sibycherian@users.noreply.github.com>
2023-05-18 00:07:06 +0530

    Add files via upload
    Day1 HW

---
+++ Smartphone.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iphone I = new Iphone();
+		I.makecall();
+		I.model();
+	}
+
+}
